cd6e58af3a5b2fbcde66510d35107e32922a26a0
ua.karnilchyk <v.kornilchik2015@gmail.com>
2022-02-01 14:31:06 +0300

    HomeWork_Lecture_6

---
+++ src/test/java/Lecture_6/Task_6.java
@@ -0,0 +1,62 @@
+package Lecture_6;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.openqa.selenium.support.locators.RelativeLocator.with;
+
+public class Task_6 {
+    WebDriver driver = null;
+
+    @BeforeTest
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void testSauceDemo_MainLocators() {
+        driver.get("https://www.saucedemo.com/");
+        //WebElements
+        WebElement inputUsername = driver.findElement(By.id("user-name"));
+        WebElement inputPassword = driver.findElement(By.name("password"));
+        WebElement buttonLogin = driver.findElement(By.cssSelector(".submit-button.btn_action"));
+
+        inputUsername.sendKeys("standard_user");
+        inputPassword.sendKeys("secret_sauce");
+        buttonLogin.click();
+
+        String nameOfProduct = driver.findElement(By.xpath("//div[@class='inventory_list']/div[2]//following::div[3]")).getText();
+        WebElement priceOfProduct = driver.findElement(By.xpath("//div[@class='inventory_list']/div[2]//div[2]//div[@class='inventory_item_price']"));
+        WebElement buttonAddToCard = driver.findElement(with(By.tagName("button")).toRightOf(priceOfProduct));
+        WebElement productCard = driver.findElement(By.className("shopping_cart_container"));
+        List<String> dataOnPage = new ArrayList<String>() {{
+            add(nameOfProduct);
+            add(priceOfProduct.getText());
+        }};
+
+        buttonAddToCard.click();
+        productCard.click();
+
+        WebElement nameOfProductInCard = driver.findElement(By.className("inventory_item_name"));
+        WebElement priceOfProductInCard = driver.findElement(By.cssSelector(".inventory_item_price"));
+
+        List<String> dataInCard = new ArrayList<String>() {{
+            add(nameOfProductInCard.getText());
+            add(priceOfProductInCard.getText());
+        }};
+
+        Assert.assertEquals(dataOnPage, dataInCard);
+
+
+    }
+
+}
