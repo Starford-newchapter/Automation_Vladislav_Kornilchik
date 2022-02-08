728a8ca28f14e9c6fd77aa3b61c762270b175ca4
ua.karnilchyk <v.kornilchik2015@gmail.com>
2022-02-08 23:00:43 +0300

    HoweWork_8_task2-remove empty lines

--- src/test/java/Lecture_8/HomeWok_8_task_2.java
+++ src/test/java/Lecture_8/HomeWok_8_task_2.java
@@ -11,48 +11,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HomeWok_8_task_2 extends BaseTest {
-
     @Test
-    public void Lecture_8_task_2(){
+    public void Lecture_8_task_2() {
         driver.get("C:\\Users\\ua.karnilchyk\\IdeaProjects\\Automation(Vladislav_Kornilchik)\\src\\test\\java\\Lecture_8\\site_check.html");
-        List<WebElement> table=driver.findElements(By.xpath("//tbody//following::tr//td[1]"));
-        WebElement userNameInput=driver.findElement(By.id("username"));
-        WebElement checkbox=driver.findElement(By.id("vehicle1"));
-        WebElement cars=driver.findElement(By.id("cars"));
-        Select selectCar=new Select(cars);
-
-        WebElement button=driver.findElement(By.xpath("//input[@type='submit']"));
-        WebElement image=driver.findElement(By.tagName("img"));
-        WebElement link=driver.findElement(By.cssSelector("a[href]"));
-
-        List<String> actualTableText=new ArrayList<>(){{
-            table.forEach((elements)->add(elements.getText()));
+        List<WebElement> table = driver.findElements(By.xpath("//tbody//following::tr//td[1]"));
+        WebElement userNameInput = driver.findElement(By.id("username"));
+        WebElement checkbox = driver.findElement(By.id("vehicle1"));
+        WebElement cars = driver.findElement(By.id("cars"));
+        Select selectCar = new Select(cars);
+        WebElement button = driver.findElement(By.xpath("//input[@type='submit']"));
+        WebElement image = driver.findElement(By.tagName("img"));
+        WebElement link = driver.findElement(By.cssSelector("a[href]"));
+        List<String> actualTableText = new ArrayList<>() {{
+            table.forEach((elements) -> add(elements.getText()));
         }};
-        List<String>expectedText=new ArrayList<>(){{
+        List<String> expectedText = new ArrayList<>() {{
             add("Alfreds Futterkiste");
             add("Centro comercial");
             add("Ernst Handel");
             add("Island Trading");
         }};
-
         userNameInput.sendKeys("Name");
         checkbox.click();
         selectCar.selectByIndex(3);
         button.click();
-        Assert.assertEquals(actualTableText,expectedText);
+        Assert.assertEquals(actualTableText, expectedText);
         Assert.assertTrue(image.isDisplayed());
-        Assert.assertEquals(driver.findElement(By.tagName("p")).getText(),"Learn how to write Html page");
-       link.click();
-       Assert.assertTrue(driver.findElement(By.xpath("//*[@rel='author']")).isDisplayed());
-
-
-
-
-
-
-
-
-
+        Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "Learn how to write Html page");
+        link.click();
+        Assert.assertTrue(driver.findElement(By.xpath("//*[@rel='author']")).isDisplayed());
 
 
     }
