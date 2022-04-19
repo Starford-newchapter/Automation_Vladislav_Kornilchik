49e9f7461780d1c8e46f6ad706656b235570f5f4
ua.karnilchyk <v.kornilchik2015@gmail.com>
2022-04-19 14:20:46 +0300

    Home_Work_16 Lombok Project done

--- src/main/java/Entinty/CheckOut.java
+++ src/main/java/Entinty/CheckOut.java
@@ -1,34 +1,26 @@
 package Entinty;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder(builderClassName = "CheckoutBuilder",setterPrefix = "with",buildMethodName = "create")
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class CheckOut {
     private String firstName;
     private String lastName;
     private String zipCode;
 
-    public CheckOut() {
-    }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
+    public static class CheckoutBuilder {
+        public CheckoutBuilder() {
+        }
     }
 }
+
+
+
--- src/main/java/Entinty/User.java
+++ src/main/java/Entinty/User.java
@@ -1,53 +1,24 @@
 package Entinty;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder(builderClassName = "Builder", setterPrefix = "with", buildMethodName = "build")
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class User {
     private String login;
     private String password;
 
-    public String getLogin() {
-        return login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public User(String login, String password) {
-        this.login = login;
-        this.password = password;
-    }
-
-    public User() {
-    }
-
     public static class Builder {
-        User user;
-
         public Builder() {
-            user = new User();
         }
-
-        public Builder withLogin(String login) {
-            user.login = login;
-            return this;
-        }
-
-        public Builder withPassword(String password) {
-            user.password = password;
-            return this;
-        }
-
-        public User build() {
-            return user;
-        }
-
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "login='" + login + '\'' +
-                ", password='" + password + '\'' +
-                '}';
     }
 }
+
+
+
